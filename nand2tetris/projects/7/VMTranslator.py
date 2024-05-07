e909c250c43217f8045f3d8cae9a2f7f7c1c17c6
Therkel <antherkelsen@gmail.com>
2024-05-07 19:30:50 +0200

    readlines function (method) created

--- nand2tetris/projects/7/VMTranslator.py
+++ nand2tetris/projects/7/VMTranslator.py
@@ -1,22 +1,53 @@
 ## How program is supposed to be run:
 # https://www.coursera.org/learn/nand2tetris2/lecture/qmJl3/unit-1-8-vm-translator-proposed-implementation
 
+#### ---- import ----
+# https://stackoverflow.com/a/37867717/3560695
+
+import re
+
+# Test packages
+import os ## Testing purposes
+
+
 ## Test stuff START
 
-import os
+# os.getcwd()
 
-os.getcwd()
+
+# filename = 'nand2tetris/projects/7/StackArithmetic/SimpleAdd/SimpleAdd.vm'
+
+
 ## Test stuff END
 
 
 
 
-## Read lines
+## Read lines method
+def readlines(filename : str, comment = "//"):
+    """
+Returns a list of the lines from an input filename. Removes comments and blank lines.
 
-filename = '/nand2tetris/projects/7/StackArithmetic/SimpleAdd/SimpleAdd.vm'
-
-file = open(filename, 'r')
-Lines = file.readlines()
+Parameters
+----
+filename : str
+    File path and file name of file to be read.
+comment : str
+    Comment identifier.
+    """
+    import re
+    file = open(filename, 'r')
+    lines = file.readlines()
+    
+    ## Remove comments
+    re_comment = re.escape(comment)
+    # print(re_comment)
+    lines_nocomment = [re.sub(r'(\s*'+re_comment+'.*)','',line) for line in lines]
+    # print(lines_nocomment)
+    ## Remove empty lines
+    lines_filtered = [line for line in lines_nocomment if line.strip()]
+    # print(lines_filtered)
+    return lines_filtered
 
 
 
