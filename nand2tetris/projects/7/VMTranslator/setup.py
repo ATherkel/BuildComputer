43645ea94302f60b4c25961ed23abfcccc477ea6
Therkel <antherkelsen@gmail.com>
2024-05-13 19:15:41 +0200

    Add Python version requirement

--- nand2tetris/projects/7/VMTranslator/setup.py
+++ nand2tetris/projects/7/VMTranslator/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='VMTranslator',
     version='0.1',
+    python_requires = '3.12.3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
