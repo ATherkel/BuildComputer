b98cdff4a64f4a9a401beb105118229e9484165d
ATherkel <at.bmxrider@gmail.com>
2024-05-14 22:33:15 +0200

    New main - and testing

--- nand2tetris/projects/7/VMTranslator/VMTranslator.py
+++ nand2tetris/projects/7/VMTranslator/VMTranslator.py
@@ -6,6 +6,8 @@
 # https://stackoverflow.com/a/37867717/3560695
 
 import importlib.util
+import sys
+import re
 
 ## Import parser
 ## We have to use import_module since the path contains an illegal directory name (7). 
@@ -18,18 +20,53 @@ Parser = parser_module.parser
 #### ---- main ----
 
 #def main():
-parser = Parser("nand2tetris/projects/7/StackArithmetic/SimpleAdd/SimpleAdd.vm")
-
-parser.hasMoreCommands()
 
 
+def main(filename):
+    
+    ## https://regex101.com/r/x0v99o/3
+    ## https://stackoverflow.com/a/59696768/3560695
+    regex = r'^(?P<path>.*[\\\/])?(?P<name>\.*.*?)(?P<extension>\.[^.]+?|)$'
+
+    filename_write = re.sub(regex, r"\1\2.asm")
+
+
+    with open(filename, 'r') as file, open(filename_write, "w"):
+        parser = Parser(file)
+
+        while parser.hasMoreCommands(): ## As long as file has more lines, do:
+            parser.advance()            ## Go to the next line in the file.
+            parser.getinstruction()     ## sets parser.instruction to current instruction
+            if not parser.instruction:  ## If instruction is blank, skip. Line consisted only of a comment. 
+                continue
+            print(f"---- Line {parser.lineNo} ----")
+            print(f"instruction = {parser.instruction}")
+            print(f"commandType() = {parser.commandType()}")
+            print(f"arg1() = {parser.arg1()}")
+            print(f"arg2() = {parser.arg2()}")
+
+            if parser.commandType() == "C_ARITHMETIC":
+                # Write arithmetic from arithmetic.asm
+                ...
+            elif parser.commandType() in ["C_PUSH", "C_POP"]:
+                # Write push and pop commands from push.asm and pop.asm
+                ...
+            else:
+                ...
+
+
+
+
+## Testing
+filename = '/nand2tetris/projects/7/StackArithmetic/SimpleAdd/SimpleAdd.vm'
+
+main(filename[1:])
 
 
 
 
 
-
-
+## Below is probably redundant
 
 
 
