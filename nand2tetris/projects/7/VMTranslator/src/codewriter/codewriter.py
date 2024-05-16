5a5d3160a685a75f1e8d91880fca8bca03c894a3
ATherkel <at.bmxrider@gmail.com>
2024-05-16 13:04:42 +0200

    Update writePushPop

--- nand2tetris/projects/7/VMTranslator/src/codewriter/codewriter.py
+++ nand2tetris/projects/7/VMTranslator/src/codewriter/codewriter.py
@@ -15,19 +15,30 @@ class codewriter:
     
     def __init__(self, file) -> None:
         self.file = file        ## functions need to be able to grab file. 
+        self.newline = '\n'
+
+        self.Parser = parser_module.parser
 
     @staticmethod
-    def writeArithmetic(command : str) -> None:
+    def writeArithmetic(command : str, ) -> None:
         """
         Arguments
             command (string)
         Function
             Writes to the output file the assembly code that implements the given arithmetic command.
             """
-        print(command)
         
-    @staticmethod
-    def writePushPop(command : str, segment : str, index : int):
+        ## Unary or binary operation:
+        arity = dicts.arithmetic[command]
+        
+        with open(f"nand2tetris/projects/7/VMTranslator/src/utils/asm/arithmetic_{arity}", 'r') as asm:
+            ...
+
+
+
+        
+        
+    def writePushPop(self, command : str, segment : str, index : int):
         """
         Arguments
         ----
@@ -39,15 +50,6 @@ class codewriter:
         Writes to the output file the assembly code that implements the given command,
         where command is either C_PUSH or C_POP.
         """
-        newline = '\n'
-
-        Parser = parser_module.parser
-
-        ## Used in some of the .asm templates.
-        ## Contains Hack name convention for segments, e.g. local is "LCL"
-        segmentPointer = dicts.segment[segment]
-
-        # Write push and pop commands from push.asm and pop.asm
         
         ## First handle 'pointer' logic translation to THIS/THAT
         if segment == "pointer":
@@ -58,6 +60,10 @@ class codewriter:
             if segment == "constant":
                 asm_location = "pushConstant.asm"
             elif segment in ["local", "argument", "this", "that", "temp"]:
+                ## Used in some of the .asm templates.
+                ## Contains Hack name convention for segments, e.g. local is "LCL"
+                segmentPointer = dicts.segment[segment]
+
                 asm_location = "pushSegment.asm"
             elif segment == "static":
                 asm_location = "pushStatic.asm"
@@ -66,15 +72,24 @@ class codewriter:
             # elif segment == "pointer": ## Handled above
             #     ...
         
-        with open("nand2tetris/projects/7/VMTranslator/src/utils/asm/pushSegment.asm", 'r') as asm:
-            parser = Parser(asm)
+        lines = [] ## Initialize lines. 
+        
+
+        with open(f"nand2tetris/projects/7/VMTranslator/src/utils/asm/{asm_location}", 'r') as asm:
+            parser = self.Parser(asm)
             
             while parser.hasMoreCommands():
                 parser.advance()
                 parser.getinstruction()
                 if not parser.instruction:  ## If instruction is blank, skip. Line consisted only of a comment. 
                     continue
+                
+                line = parser.instruction.format(**locals())
+                lines.append(line)
+                # if not parser.hasMoreCommands():
+                #     lines.append(self.newline)
         return lines
+    
 
         
     
